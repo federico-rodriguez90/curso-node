6643458bd6aacbf0bdcf0d3181f7a108f5a72ada
Federico Rodriguez <federico.rodriguez@intive.com>
2021-12-23 16:37:05 -0300

    Despiegue app 2021-23-12

--- app.js
+++ app.js
@@ -13,22 +13,22 @@ hbs.registerPartials(__dirname + "/views/partials");
 app.use(express.static("public"));
 
 // RUTAS
-app.get("/", (req, res) => {
-  res.render("home", { nombre: "Fede", titulo: "Curso Node" });
-});
-
-app.get("/generic", (req, res) => {
-  res.render("generic", { nombre: "Fede", titulo: "Curso Node" });
-  //   res.sendFile(__dirname + "/public/generic.html");
-});
-
-app.get("/elements", (req, res) => {
-  res.render("elements", { nombre: "Fede", titulo: "Curso Node" });
-  //   res.sendFile(__dirname + "/public/elements.html");
-});
-// app.get("*", (req, res) => {
-//   res.sendFile(__dirname + "/public/404.html");
+// app.get("/", (req, res) => {
+//   res.render("home", { nombre: "Fede", titulo: "Curso Node" });
 // });
+
+// app.get("/generic", (req, res) => {
+//   res.render("generic", { nombre: "Fede", titulo: "Curso Node" });
+//   //   res.sendFile(__dirname + "/public/generic.html");
+// });
+
+// app.get("/elements", (req, res) => {
+//   res.render("elements", { nombre: "Fede", titulo: "Curso Node" });
+//   res.sendFile(__dirname + "/public/elements.html");
+// });
+app.get("*", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
 
 app.listen(port, () => {
   console.log(`Escuchando el puerto ${port}...`);
